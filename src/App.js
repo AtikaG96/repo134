3a7afcdf735ccd3c038277b960d9da955a4bde8c
AtikaG96 <atikapersonalalbum@gmail.com>
2026-07-07 11:41:53 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,42 +1,38 @@
 import React from 'react';
 import { HashRouter, Routes, Route } from 'react-router-dom';
+
 import { ProfileProvider } from './ProfileContext';
+
 import Home from './pages/Home';
-import Navbar from './assets/components/Navbar';
 import About from './pages/About';
 import MyProjects from './pages/MyProjects';
 import Experience from './pages/Experience';
 import Education from './pages/Education';
 import Contact from './pages/Contact';
 import Certifications from './pages/Certifications';
+
+import Navbar from './assets/components/Navbar';
+
 import './GlobalStyles.css';
 
 const App = () => {
   return (
     <ProfileProvider>
-      <div
-        style={{
-          height: '100vh',
-          overflowY: 'auto',
-          display: 'flex',
-          flexDirection: 'column',
-        }}
-      >
-        <HashRouter basename="/">
-          <Navbar />
-          <div style={{ flex: 1 }}>
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/about" element={<About />} />
-              <Route path="/myprojects" element={<MyProjects />} />
-              <Route path="/experience" element={<Experience />} />
-              <Route path="/education" element={<Education />} />
-              <Route path="/contact" element={<Contact />} />
-              <Route path="/certifications" element={<Certifications />} />
-            </Routes>
-          </div>
-        </HashRouter>
-      </div>
+      <HashRouter>
+        <Navbar />
+
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/myprojects" element={<MyProjects />} />
+            <Route path="/experience" element={<Experience />} />
+            <Route path="/education" element={<Education />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/certifications" element={<Certifications />} />
+          </Routes>
+        </main>
+      </HashRouter>
     </ProfileProvider>
   );
 };
